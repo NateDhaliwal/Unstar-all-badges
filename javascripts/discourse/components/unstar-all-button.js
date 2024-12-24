88feb1dd0123488ce353cbea85ec2144d63e9b78
NateDhaliwal <143097364+NateDhaliwal@users.noreply.github.com>
2024-12-24 16:20:45 +0800

    Update unstar-all-button.js

--- javascripts/discourse/components/unstar-all-button.js
+++ javascripts/discourse/components/unstar-all-button.js
@@ -4,9 +4,9 @@ import { action } from "@ember/object";
 
 export default class UnstarAllButton extends Component {
   @action
-  unstarAll() {
+  async unstarAll() {
     const user_name = require("discourse/models/user").default.current().username;
-    const badges = require("discourse/models/user-badge").default.findByUsername(user_name);
+    const badges = await require("discourse/models/user-badge").default.findByUsername(user_name);
     const favorites = new Map();
     badges.filter((b)=>b.is_favorite).forEach((b)=>favorites.set(b.badge_id,b));
     favorites.forEach((b)=>b.favorite());
